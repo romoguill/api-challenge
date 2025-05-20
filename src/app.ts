ed4220dac9fac51a9c5d604b0065acf84931a385
romoguill <romoguill@gmail.com>
2025-05-20 17:45:36 -0300

    Error in render

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,6 @@ app.post('/teapot', (req: Request, res: Response) => {
 });
 
 // Start server
-app.listen(port, () => {
+app.listen(Number(port), '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
